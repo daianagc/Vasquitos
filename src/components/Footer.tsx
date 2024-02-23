a54378ca3c9597fa1cb79333478e5d78cdba3dc9
Daiana cucco <dgcucco91@gmail.com>
2024-02-23 12:55:28 -0300

    Agregue nuevos estilos

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,36 +10,44 @@ const Footer = () => {
     <section>
       <div className="container-Footer">
         <footer className="footer">
-          <div className="container-Icons">
-            <i>
-              <Link className="instagram" to="https://www.instagram.com/">
-                <img
-                  className="Icon-instagram"
-                  src={iconInstagram}
-                  alt="Icon"
-                />
-              </Link>
-            </i>
-            <i>
-              <Link className="facebook" to="https://www.facebook.com/">
-                <img className="Icon-facebook" src={iconFacebook} alt="Icon" />
-              </Link>
-            </i>
-            <i>
-              <Link className="tiktok" to="https://www.tiktok.com/es/">
-                <img className="Icon-tiktok" src={iconTiktok} alt="Icon" />
-              </Link>
-            </i>
-          </div>
-          <div className="texto-Footer">
-            <p className="Parrafo-Footer">© Vascos Animalistas 2024.</p>
+          <div className="container">
+            <div className="container-Icons">
+              <i>
+                <Link className="instagram" to="https://www.instagram.com/">
+                  <img
+                    className="Icon-instagram"
+                    src={iconInstagram}
+                    alt="Icon"
+                  />
+                </Link>
+              </i>
+              <i>
+                <Link className="facebook" to="https://www.facebook.com/">
+                  <img
+                    className="Icon-facebook"
+                    src={iconFacebook}
+                    alt="Icon"
+                  />
+                </Link>
+              </i>
+              <i>
+                <Link className="tiktok" to="https://www.tiktok.com/es/">
+                  <img className="Icon-tiktok" src={iconTiktok} alt="Icon" />
+                </Link>
+              </i>
+            </div>
+            <div className="texto-Footer">
+              <p>Av. 14 n°2598</p>
+              <p>vascosanimalistas@gmail.com</p>
+              <p>2255-2324546</p>
+            </div>
           </div>
           <div className="Mapa">
             <MyMap
-              height={118}
+              height={150}
               defaultCenter={[-37.27046112725326, -56.993164051984074]}
-              defaultZoom={2.5}
-              width={350}
+              defaultZoom={15}
+              width={370}
               anchor={[-37.27046112725326, -56.993164051984074]}
               markerWidth={60}
             />
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,17 @@
 import { Link, NavLink } from "react-router-dom";
 import "../styles/Navbar.css";
 import logoVascos from "../img/VASCOS-ANIMALISTAS.ico";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <nav className="nav-list">
-      <div className="div">
-        <div className="container-Logo">
-          <img className="logoVascos" src={logoVascos} alt="LogoVascos" />
-        </div>
-        <ul className="ul-list">
+      <div className="container-Logo">
+        <img className="logoVascos" src={logoVascos} alt="LogoVascos" />
+      </div>
+      <div className={`nav-items ${isOpen && "open"}`}>
+        <ul className="ul-list" style={{ display: isOpen ? "" : "none" }}>
           <li>
             <Link className="nav-link" to="/">
               Inicio
@@ -47,6 +49,14 @@ const Navbar = () => {
           <p>DONAR AHORA</p>
         </NavLink>
       </button>
+      <div
+        className={`nav-toggle ${isOpen && "open"}`}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
     </nav>
   );
 };
