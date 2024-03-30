b4e2a108c2cadffd2ece11d593157e9f18523abf
Mauricio Cucco <mscucco@hotmail.com>
2024-03-30 10:52:47 -0300

    Fix scroll restoration

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => {
           <Outlet />
         </main>
         <Footer />
-        <ScrollRestoration />
+        {/* <ScrollRestoration /> */}
       </QueryClientProvider>
     </>
   );
--- src/pages/home/components/SponsorsSection/SponsorsSection.tsx
+++ src/pages/home/components/SponsorsSection/SponsorsSection.tsx
@@ -2,19 +2,13 @@ import { useInView } from "react-intersection-observer";
 import AutoplayCarousel from "../../../../components/Carrusel/AutoplayCarousel";
 import "./SponsorsSection.css";
 import { useSpring, animated } from "@react-spring/web";
+import { fadeInLeftAnimation } from "../../../../animations";
 
 const SponsorsSection = () => {
   const [ref, inView] = useInView({
     triggerOnce: true, // Change this to false if you want the animation to trigger again whenever it comes in view
   });
-  const fadeInFromLeft = useSpring({
-    from: { opacity: 0, transform: "translate3d(-100%,0,0)" },
-    to: {
-      opacity: inView ? 1 : 0,
-      transform: inView ? "translate3d(0%,0,0)" : "translate3d(-100%,0,0)",
-    },
-    config: { duration: 2000 }, // Configure the duration of the animation
-  });
+  const fadeInFromLeft = useSpring(fadeInLeftAnimation(inView));
 
   return (
     <section ref={ref} className="sponsors-section" id="patrocinadores">
