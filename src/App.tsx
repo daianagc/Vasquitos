bad3bc57b5cc0dadc5fd4ff8d36b2be5fd252cc4
Mauricio Cucco <mscucco@hotmail.com>
2024-03-30 10:53:02 -0300

    Fix import

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import "./styles/App.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Outlet, ScrollRestoration } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 const App = () => {
   const queryClient = new QueryClient();
@@ -15,7 +15,6 @@ const App = () => {
           <Outlet />
         </main>
         <Footer />
-        {/* <ScrollRestoration /> */}
       </QueryClientProvider>
     </>
   );
