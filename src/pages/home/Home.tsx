3fdafeaaed86dfe86c88b859b31b73137c8ed085
Mauricio Cucco <mscucco@hotmail.com>
2024-03-30 10:35:23 -0300

    Remove lazy loading

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,27 +1,16 @@
-import { Suspense, lazy } from "react";
-import { Spinner } from "../../components/Spinner/Spinner";
-
-const DonationsSection = lazy(
-  () => import("./components/DonationsSection/DonationsSection")
-);
-const OurDogsSection = lazy(
-  () => import("./components/OurDogsSection/OurDogsSection")
-);
-const PresentationSection = lazy(
-  () => import("./components/PresentationSection/PresentationSection")
-);
-const SponsorsSection = lazy(
-  () => import("./components/SponsorsSection/SponsorsSection")
-);
+import DonationsSection from "./components/DonationsSection/DonationsSection";
+import OurDogsSection from "./components/OurDogsSection/OurDogsSection";
+import PresentationSection from "./components/PresentationSection/PresentationSection";
+import SponsorsSection from "./components/SponsorsSection/SponsorsSection";
 
 const Home = () => {
   return (
-    <Suspense fallback={<Spinner backgroundColor="#000000" />}>
+    <>
       <PresentationSection />
       <OurDogsSection />
       <DonationsSection />
       <SponsorsSection />
-    </Suspense>
+    </>
   );
 };
 
